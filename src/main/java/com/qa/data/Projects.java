6a456a87cb91567a6bf69834ac2b29f0a5d1e68c
KDiggory <katie.diggory@gmail.com>
2021-10-10 19:47:08 +0100

    Projects class

--- src/main/java/com/qa/data/Projects.java
+++ src/main/java/com/qa/data/Projects.java
@@ -1,5 +1,93 @@
 package com.qa.data;
 
-public class Projects {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
 
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Projects {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer projectId;
+	
+	private String projectName;
+	
+	private String projectMaterials;
+	
+	private boolean easy;
+	
+	private Integer days;
+	
+	
+	@ManyToOne
+	private Workshop workshop;
+
+
+	public Projects(Integer projectId, String projectName, String projectMaterials, boolean easy, Integer days,
+			Workshop workshop) {
+		super();
+		this.projectId = projectId;
+		this.projectName = projectName;
+		this.projectMaterials = projectMaterials;
+		this.easy = easy;
+		this.days = days;
+		this.workshop = workshop;
+	}
+	
+	public Projects() { // REQUIRED default constructor
+		super();
+	}
+
+	public Integer getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(Integer projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getProjectMaterials() {
+		return projectMaterials;
+	}
+
+	public void setProjectMaterials(String projectMaterials) {
+		this.projectMaterials = projectMaterials;
+	}
+
+	public boolean isEasy() {
+		return easy;
+	}
+
+	public void setEasy(boolean easy) {
+		this.easy = easy;
+	}
+
+	public Integer getDays() {
+		return days;
+	}
+
+	public void setDays(Integer days) {
+		this.days = days;
+	}
+
+	public Workshop getWorkshop() {
+		return workshop;
+	}
+
+	public void setWorkshop(Workshop workshop) {
+		this.workshop = workshop;
+	}
+	
 }
