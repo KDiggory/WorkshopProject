7fb1e6e8a5d393b13f0fbf4fa7db87136aa50aef
KDiggory <katie.diggory@gmail.com>
2021-10-10 21:43:11 +0100

    Workshop Controller and Service classes

--- src/main/java/com/qa/controller/WorkshopController.java
+++ src/main/java/com/qa/controller/WorkshopController.java
@@ -1,5 +1,61 @@
 package com.qa.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.data.Workshop;
+import com.qa.service.WorkshopService;
+
+@RestController
 public class WorkshopController {
+	
+	private WorkshopService service;
+	
+	public WorkshopController(WorkshopService service) {
+		super();
+		this.service = service;
+	}
+	
+	@GetMapping("/getWoprkshopById/{id}") 
+	public Optional<Workshop> getByIndex(@PathVariable Integer id) {
+		return this.service.getByIndex(id);	
+	}
+	
+	@GetMapping("/getAllWoprkshops") 
+	public List<Workshop> getAllIds() {
+		return this.service.getAllIds();	
+	}
+	
+	@PostMapping("/createWorkshop") 
+	public ResponseEntity<Workshop> createWorkShop(@RequestBody Workshop workshop) {
+	Workshop responseBody = this.service.createWorkshop(workshop);
+	ResponseEntity<Workshop> response = new ResponseEntity<Workshop>(responseBody, HttpStatus.CREATED);
+	return response;
+	}
+	@PutMapping("/updateWorkshop/{id}")
+	public ResponseEntity<Workshop> updateWorkshop(@RequestBody Workshop workshop, @PathVariable Integer id) {
+	Workshop responseBody = this.service.updateWorkshop(workshop, id);
+	ResponseEntity<Workshop> response = new ResponseEntity<Workshop>(responseBody, HttpStatus.ACCEPTED);
+	return response;	
+	}
+	
+	@DeleteMapping("/removeWorkshop/{id}")
+	public ResponseEntity<Workshop> deleteWorkshop(@PathVariable Integer id) {
+	this.service.deleteWorkshop(id);
+	ResponseEntity<Workshop> response = new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	return response;
+		}
+	
+	
 
 }
